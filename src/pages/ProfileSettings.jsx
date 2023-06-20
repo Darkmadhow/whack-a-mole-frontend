b404fea72d49509f44d10481cdabc8ef6fbad155
Friis Kraas <friis.kraas@gmx.de>
2023-06-20 09:59:29 +0200

    added settings link to navbar

--- src/pages/ProfileSettings.jsx
+++ src/pages/ProfileSettings.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import NavBar from "../components/NavBar";
+import { UserContext } from "../userContext";
+import "../styles/profileSettings.css";
 
 export default function ProfileSettings() {
-  return <div>profile Settings</div>;
+  const { user } = useContext(UserContext);
+
+  return (
+    <div className="profile-page">
+      <NavBar />
+      <div className="settings-container">
+        <h2>Profile Settings</h2>
+        <form>
+          <label>{user?.email}</label>
+          <button onClick={toggleEmail}>Change Email</button>
+        </form>
+        <form></form>
+      </div>
+    </div>
+  );
 }
