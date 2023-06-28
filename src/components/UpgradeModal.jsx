335f31f5452661ef374d0dde0dc29bfa9d380e4d
Friis <friis.kraas@gmx.de>
2023-06-28 16:18:35 +0200

    added modal for upgrade selection

--- src/components/UpgradeModal.jsx
+++ src/components/UpgradeModal.jsx
@@ -1,22 +1,39 @@
 import React from "react";
-import '../styles/UpgradeModal.css';
+import "../styles/UpgradeModal.css";
 
-export default function UpgradeModal({stage, gameObserver, subtractLife}) {
-    function onClick(){
-        gameObserver.current.emit('reset');
-        stage.start();
-        gameObserver.current.on("evaded",subtractLife);
-    }
+export default function UpgradeModal({
+  stage,
+  gameObserver,
+  subtractLife,
+  option1,
+  option2,
+  handleUpgradeSelection,
+}) {
+  function onClick(userChoice) {
+    gameObserver.current.emit("reset");
+    stage.start();
+    gameObserver.current.on("evaded", subtractLife);
+    handleUpgradeSelection(userChoice);
+  }
   return (
     <div className="upgrade-modal">
       <dialog id="my_modal_2" className="modal">
         <form method="dialog" className="modal-box">
           <h2 className="font-bold text-lg">Choose an Upgrade!</h2>
-          <p className="py-4">Press ESC key or click outside to close</p>
-        </form>
-        <form method="dialog" className="modal-backdrop">
-          <button onClick={onClick}>Upgrade 1</button>
-          <button onClick={onClick}>Upgrade 2</button>
+          <div className="modal-action">
+            <button
+              onClick={() => onClick(option1)}
+              style={{ backgroundImage: `url(${option1.asset})` }}
+            >
+              {option1.name}
+            </button>
+            <button
+              onClick={() => onClick(option2)}
+              style={{ backgroundImage: `url(${option2.asset})` }}
+            >
+              {option2.name}
+            </button>
+          </div>
         </form>
       </dialog>
     </div>
--- src/pages/StandardGame.jsx
+++ src/pages/StandardGame.jsx
@@ -9,24 +9,51 @@ import { NavLink } from "react-router-dom";
 import { Stage, Sprite, Container } from "@pixi/react";
 import { Texture, Graphics } from "pixi.js";
 import { EventEmitter } from "@pixi/utils";
-import "../styles/game.css";
+import { UserContext } from "../userContext";
+import { uploadHighScore } from "../utils/scores";
+import UpgradeModal from "../components/UpgradeModal";
 import MoleHole from "../assets/game/MoleHole";
 import MoleContainer from "../assets/game/MoleContainer";
 import MalletStandard from "../assets/game/MalletStandard";
-import { uploadHighScore } from "../utils/scores";
-import { UserContext } from "../userContext";
 import Reticle from "../assets/game/Reticle";
-import UpgradeModal from "../components/UpgradeModal";
+import rocketHammer from "../assets/img/mallet_rocket.png";
+import spikeHammer from "../assets/img/mallet_spikey.png";
+import droneHammer from "../assets/img/drone.png";
+import bomb from "../assets/img/bomb.png";
+import cover from "../assets/img/cover.png";
+import trap from "../assets/img/trap.png";
+import "../styles/game.css";
 
 export default function StandardGame() {
+  /* ------------------------- INITIAL VALUES SETUP ------------------------- */
+  /* ------------------------- -------------------- ------------------------- */
+  //the stage component including all our sprites
   const [stage, setStage] = useState();
   //the event emitter that will handle all game interactions
   const gameObserver = useRef(new EventEmitter());
-  //initial score and lives
+  //initial score, lives and difficulty level
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(5);
+  const [level, setLevel] = useState(1);
   //difficulty speed multiplier
   const haste = useRef(1);
+  //the upgrades chosen by the user stored as string array and available upgrades
+  const [chosenUpgrades, setChosenUpgrades] = useState([]);
+  const [availableHammerUpgrades, setAvailableHammerUpgrades] = useState([
+    { name: "rocket_hammer", asset: rocketHammer },
+    { name: "spike_hammer", asset: spikeHammer },
+  ]);
+  const [[option1, option2], setOptions] = useState([
+    { name: null, asset: null },
+    { name: null, asset: null },
+  ]);
+  const [availableDeployableUpgrades, setAvailableDeployableUpgrades] =
+    useState([
+      { name: "bomb", asset: bomb },
+      { name: "cover", asset: cover },
+      { name: "trap", asset: trap },
+      { name: "drone", asset: droneHammer },
+    ]);
 
   //subscribe to mole events
   const { token } = useContext(UserContext);
@@ -41,6 +68,8 @@ export default function StandardGame() {
     };
   }, []);
 
+  /* ------------------------------ SCORING FUNCTIONS ------------------------------ */
+  /* ------------------------------ ----------------- ------------------------------ */
   function updateScore(e) {
     setScore((prev) => prev + e.value);
   }
@@ -49,6 +78,8 @@ export default function StandardGame() {
     setLives((prev) => prev - 1);
   }, [lives]);
 
+  /* ------------------------------ MOLE HANDLING ------------------------------ */
+  /* ------------------------------ ------------- ------------------------------ */
   //counter for the moles in each hole, as iterable object
   const [mole_count, setMoleCount] = useState({
     0: 0,
@@ -101,7 +132,8 @@ export default function StandardGame() {
     setMoles(molesTemp);
   }
 
-  //--------------- Stage Settings ---------------
+  /* ----------------------------------- STAGE SETTINGS ----------------------------------- */
+  /* ----------------------------------- -------------- ----------------------------------- */
   const stageProps = {
     height: 900,
     width: 1400,
@@ -109,6 +141,7 @@ export default function StandardGame() {
       backgroundAlpha: 0,
     },
   };
+
   const hole_coords = [
     { x: 300, y: 200 },
     { x: 600, y: 200 },
@@ -116,6 +149,7 @@ export default function StandardGame() {
     { x: 400, y: 400 },
     { x: 700, y: 400 },
   ];
+
   const hole_masks = useRef({
     0: drawCircleMask(hole_coords[0].x, hole_coords[0].y),
     1: drawCircleMask(hole_coords[1].x, hole_coords[1].y),
@@ -123,8 +157,81 @@ export default function StandardGame() {
     3: drawCircleMask(hole_coords[3].x, hole_coords[3].y),
     4: drawCircleMask(hole_coords[4].x, hole_coords[4].y),
   });
-  //--------------- Stage Settings End ---------------
 
+  /* ------------------------------ UPGRADE FUNCTIONALITY ------------------------------ */
+  /* ------------------------------ --------------------- ------------------------------ */
+
+  /*
+   * increases difficulty over time and triggers upgrade selection
+   */
+  useEffect(() => {
+    //count moles hit
+    const molecounter =
+      mole_count[0] +
+      mole_count[1] +
+      mole_count[2] +
+      mole_count[3] +
+      mole_count[4] +
+      1;
+
+    //increase difficulty every 10 moles, open modal to offer an upgrade
+    if (!(molecounter % 10)) {
+      gameObserver.current.off("evaded", subtractLife);
+      haste.current *= 1.03;
+      setLevel((prev) => prev + 1);
+      gameObserver.current.emit("reset_incoming");
+      stage.stop();
+      setOptions(getUpgradeOptions());
+      setTimeout(() => {
+        window.my_modal_2.showModal();
+      }, 1500);
+    }
+  }, [mole_count]);
+
+  function getUpgradeOptions() {
+    //no hammer upgade chosen? offer hammers first
+    if (availableHammerUpgrades.length > 1) {
+      const [a, b] = getRandomIndices(availableHammerUpgrades);
+      const optionA = availableHammerUpgrades[a];
+      const optionB = availableHammerUpgrades[b];
+      //allow only one hammer upgrade: empty array upon offering the choice
+      setAvailableHammerUpgrades([]);
+      return [optionA, optionB];
+    }
+    //if less than 2 hammer upgrades are left, offer deployable object instead
+    const [a, b] = getRandomIndices(availableDeployableUpgrades);
+    const optionA = availableDeployableUpgrades[a];
+    const optionB = availableDeployableUpgrades[b];
+    //if no upgrades are left, return empty upgrades
+    if (!optionA && !optionB)
+      return [
+        { name: "No Upgrade left", asset: null },
+        { name: "No Upgrade left", asset: null },
+      ];
+
+    return [optionA, optionB];
+  }
+
+  /*
+   * handleUpgradeSelection: The callback being executed by the upgrade choice modal
+   * params: userChoice, the Upgrade Object the user has clicked
+   * return: returns an array of 2 upgrade Objects
+   */
+  function handleUpgradeSelection(userChoice) {
+    //if no upgrades are left, we get an empty asset, don't to anything
+    if (userChoice.asset == null) return;
+
+    setChosenUpgrades((prev) => [...prev, userChoice]);
+    if (availableDeployableUpgrades.includes(userChoice)) {
+      const index = availableDeployableUpgrades.indexOf(userChoice);
+      setAvailableDeployableUpgrades(
+        availableDeployableUpgrades.toSpliced(index, 1)
+      );
+    }
+  }
+
+  /* ------------------------------ HELPER FUNCTIONS ------------------------------ */
+  /* ------------------------------ ---------------- ------------------------------ */
   /*
    * drawCircleMask: draws a circular Graphics object with red fill
    * params: x,y define the position and the center of the circle
@@ -140,31 +247,45 @@ export default function StandardGame() {
     return newCircle;
   }
 
+  // Function to generate a random number within a range
+  function getRandomIndex(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   /*
-   * increases difficulty over time
+   * getRandomIndices: select 2 random indices from an array which are guaranteed to be different
+   * params: array, the array to pick two indices from
+   * returns: [index1, index2], an array with the 2 selected indices which are distinct
    */
-  useEffect(() => {
-    //count moles hit
-    const molecounter =
-      mole_count[0] +
-      mole_count[1] +
-      mole_count[2] +
-      mole_count[3] +
-      mole_count[4] +
-      1;
+  const getRandomIndices = (array) => {
+    const length = array.length;
 
-    //increase difficulty every 10 moles
-    if (!(molecounter % 10)) {
-      gameObserver.current.off("evaded", subtractLife);
-      haste.current *= 1.03;
-      gameObserver.current.emit("reset_incoming");
-      stage.stop();
-      window.my_modal_2.showModal();
+    if (length == 0) {
+      // If the array has no entries, return false in a safe way
+      return [false, false];
     }
-  }, [mole_count]);
+    if (length < 2) {
+      // If the array has less than 2 entries, return the only available index twice
+      return [0, 0];
+    }
 
+    // Generate the first random index
+    let index1 = getRandomIndex(0, length - 1);
+
+    // Generate the second random index, ensuring it is distinct from the first index
+    let index2;
+    do {
+      index2 = getRandomIndex(0, length - 1);
+    } while (index2 === index1);
+
+    return [index1, index2];
+  };
+
+  /* ------------------------------ VISUAL OUTPUT ------------------------------ */
+  /* ------------------------------ ------------- ------------------------------ */
   //game over at 0 lives
   if (lives <= 0) {
+    //upload Highscore to the backend
     uploadHighScore(token, { score: score, gamemode: "standard" });
 
     return (
@@ -187,6 +308,7 @@ export default function StandardGame() {
         <NavLink to="/modeselection">Back</NavLink>
         <div className="score-display">Score: {score}</div>
         <div className="lives">Lives: {lives}</div>
+        <div className="level">Stage: {level}</div>
       </div>
       <Stage {...stageProps} onMount={setStage}>
         <Container sortableChildren={true}>
@@ -284,6 +406,9 @@ export default function StandardGame() {
         stage={stage}
         gameObserver={gameObserver}
         subtractLife={subtractLife}
+        option1={option1}
+        option2={option2}
+        handleUpgradeSelection={handleUpgradeSelection}
       />
     </div>
   );
